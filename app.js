4388c56f280e9d1535d8daec7c2c5a221bc0dc9b
Mauricio Klein <mauricio.klein@blacklane.com>
2018-07-01 13:26:14 +0200

    Implements POST /game/:algorithm, used to calculate the winner in a game

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const gameRouter = require('./src/routes/game');
 
 const app = express();
@@ -10,6 +11,6 @@ app.set('view engine', 'pug');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/game', gameRouter);
+gameRouter(app)
 
 module.exports = app;
--- src/routes/game.js
+++ src/routes/game.js
@@ -1,8 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const GameDecorator = require('../../src/decorators/game');
 
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
+const router = (app) => {
+  const route = express.Router()
+
+  app.use('/game', route)
+
+  route.post('/:algorithm', (req, res, next) => {
+    const { algorithm } = req.params;
+    const { p1_choice, p2_choice } = req.body
+    const decorator = new GameDecorator(algorithm, p1_choice, p2_choice)
+
+    res.setHeader('Content-Type', 'application/json')
+    res.send(JSON.stringify(decorator.result()))
+  })
+
+  route.get('/', (req, res, next) => {
+    res.render('index');
+  });
+}
 
 module.exports = router;
