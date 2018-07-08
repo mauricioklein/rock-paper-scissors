2b3e20c3d62be36fe4f27a7d0ed5edbae3e89fae
Mauricio Klein <mauricio.klein@blacklane.com>
2018-07-08 18:49:09 +0200

    Layout improvements

--- public/javascripts/src/callbacks.js
+++ public/javascripts/src/callbacks.js
@@ -1,44 +1,59 @@
 (() => {
   const gameType = $('#gameType').val()
-  const elements = {
-    optionsBar: $('#optionsBar'),
-    result: {
-      container: $('#result'),
-      player1Choice: $('.scorebox > #player-1-choice'),
-      player2Choice: $('.scorebox > #player-2-choice'),
-      winner: $('.scorebox > #winner')
-    }
+  const dom = {
+    p2: {
+      container: $('#p2-container'),
+      choice: $('#p2-container > #choice')
+    },
+    winner: $('#winner'),
+    result: $('#result'),
+    newGame: $('#new-game')
   }
+
+  const imgPath = (imgName) => (
+    `/images/game-options/${imgName}.png`
+  )
 
   const callApi = (p1Choice = null, p2Choice = null) => (
     window.ApiProxy.call(gameType, p1Choice, p2Choice)
   )
 
   const showResult = (data) => {
-    elements.result.player1Choice.text(`You choose ${data.player_1_choice}`)
-    elements.result.player2Choice.text(`The oponent choose ${data.player_2_choice}`)
+    presentSelectedOption(data.player_1_choice)
 
-    switch(data.winner) {
-      case "Player 1":
-        elements.result.winner.text("You won \\o/")
-        break
-
-      case "Player 2":
-        elements.result.winner.text("You lost :(")
-        break
-
-      default:
-        elements.result.winner.text("It's a draw")
-        break
-    }
-
-    elements.optionsBar.addClass('hidden')
-    elements.result.container.removeClass('hidden')
+    dom.p2.choice.attr('src', imgPath(data.player_2_choice))
+    dom.winner.text(winnerLine(data.winner))
+    dom.p2.container.removeClass('hidden')
+    dom.result.removeClass('hidden')
   }
 
   const startNewGame = () => {
-    elements.optionsBar.removeClass('hidden')
-    elements.result.container.addClass('hidden')
+    presentAllOptions()
+    dom.p2.container.addClass('hidden')
+    dom.result.addClass('hidden')
+  }
+
+  const winnerLine = (winner) => {
+    switch(winner) {
+      case "Player 1":
+        return "You won!"
+
+      case "Player 2":
+        return "You lost!"
+
+      default:
+        return "Draw!"
+    }
+  }
+
+  const presentSelectedOption = (selectedOption) => {
+     $('[id^=gameOption-]')
+      .filter((_, el) => el.value !== selectedOption)
+      .addClass('hidden')
+  }
+
+  const presentAllOptions = () => {
+    $('[id^=gameOption-]').removeClass('hidden')
   }
 
   // Options callback
@@ -48,7 +63,7 @@
   })
 
   // New game button callback
-  $('#newGame').on('click', () => {
+  dom.newGame.on('click', () => {
     startNewGame()
   })
 })();
