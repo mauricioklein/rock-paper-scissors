69c21c1b3e4bb41736b786defcef496a92f429e9
Mauricio Klein <mauricio.klein@blacklane.com>
2018-07-08 13:52:59 +0200

    Add callbacks and api proxy snippets to public folder
    These scripts are responsible to control the entire UI workflow and 
    contact the API with a game set

---
+++ public/javascripts/src/api-proxy.js
@@ -0,0 +1,18 @@
+((win) => {
+  win.ApiProxy = {
+    call: (gameType, p1Choice, p2Choice) => {
+      const body = {
+        p1_choice: p1Choice,
+        p2_choice: p2Choice
+      };
+
+      return fetch(`/game/${gameType}`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body)
+      })
+      .then(resp => resp.json())
+      .catch(err => console.error(err));
+    }
+  };
+})(window);
