f7daaa68c19f81dd7c2d7963dbe0959a5544aefb
Mauricio Klein <mauricio.klein@blacklane.com>
2018-07-16 00:05:28 +0200

    Move DOM selectors to separated class

---
+++ public/javascripts/src/selector.js
@@ -0,0 +1,18 @@
+class Selector {
+  static get title() { return document.querySelector("#title") }
+
+  static get player1Container() { return document.querySelector("#player1-container") }
+
+  static get optionsSelector() { return document.querySelectorAll("[id^=gameOption-]") }
+
+  static get player2Container() { return document.querySelector("#player2-container") }
+  static get player2Choice() { return document.querySelector("#player2-container > #choice") }
+
+  static get resultContainer() { return document.querySelector("#result-container") }
+  static get resultWinner() { return document.querySelector("#winner") }
+
+  static get newGameButton() { return document.querySelector("#new-game") }
+  static get gameType() { return document.querySelector("#gameType") }
+}
+
+module.exports = Selector
